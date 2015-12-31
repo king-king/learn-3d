ee17895c48be1d70b18654741864f114c118ee0d
WQ <niuyiwangqun@sohu.com>
2015-12-31 14:51:59 +0800

    c

--- jellyfish/initWin.js
+++ jellyfish/initWin.js
@@ -3,33 +3,33 @@
  */
 // JavaScript Document<script type="text/javascript">
 var gl;
-var canvas,docWidth,docHeight;
+var canvas , docWidth , docHeight;
 
-function initWin(w,h){
+function initWin( w , h ) {
 
-    (w) ? docWidth = w : docWidth = $(window).width();
-    (h) ? docHeight = h : docHeight = $(window).height();
-    $("#webgl-canvas").width(docWidth);
-    $("#webgl-canvas").height(docHeight);
+    (w) ? docWidth = w : docWidth = $( window ).width();
+    (h) ? docHeight = h : docHeight = $( window ).height();
+    $( "#webgl-canvas" ).width( docWidth );
+    $( "#webgl-canvas" ).height( docHeight );
     canvas.width = docWidth;
     canvas.height = docHeight;
     try {
-        gl = canvas.getContext( "experimental-webgl" ) ;
+        gl = canvas.getContext( "experimental-webgl" );
         gl.viewportWidth = docWidth;
         gl.viewportHeight = docHeight;
-    } catch(e) {
+    } catch ( e ) {
     }
-    if (!gl) {
-        alert("Your browser failed to initialize WebGL.");
+    if ( !gl ) {
+        alert( "Your browser failed to initialize WebGL." );
     }
 }
 
-$(window).resize(function() {
+$( window ).resize( function () {
     initWin();
-});
+} );
 
 function webGLStart() {
-    canvas = document.getElementById("webgl-canvas");
+    canvas = document.getElementById( "webgl-canvas" );
 
     initWin();
     initBuffers();
@@ -38,12 +38,12 @@ function webGLStart() {
 
     setDebugParam();
 
-    gl.clearColor(0., 0., 0., 0.);
-    gl.clearDepth(1.);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.enable(gl.BLEND);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
+    gl.clearColor( 0. , 0. , 0. , 0. );
+    gl.clearDepth( 1. );
+    gl.blendFunc( gl.SRC_ALPHA , gl.ONE_MINUS_SRC_ALPHA );
+    gl.enable( gl.BLEND );
+    gl.enable( gl.DEPTH_TEST );
+    gl.depthFunc( gl.LEQUAL );
 
     interact();
     animate();
