aaf145f01a064b69a8736e39c5a7251314d77fd9
WQ <niuyiwangqun@sohu.com>
2016-01-05 14:55:28 +0800

    c

--- jellyfish/debug.js
+++ jellyfish/debug.js
@@ -4,15 +4,15 @@
 var localParam = new LocalParam();
 var Param = new param();
 
-function LocalParam(){
+function LocalParam() {
     this.camera = new Object;
     this.camera.near = 5;
     this.camera.far = 530;
     this.camera.fov = 27;
 
-    this.camera.rotate = [-0.3,0,0];
-    this.camera.translate = [0,0,-280];
-    this.camera.eye = [0,0,-64];
+    this.camera.rotate = [ -0.3 , 0 , 0 ];
+    this.camera.translate = [ 0 , 0 , -280 ];
+    this.camera.eye = [ 0 , 0 , -64 ];
 
     this.LODBias = 8.0;
     this.millis = 0.0;
@@ -25,7 +25,7 @@ function LocalParam(){
     this.cycle32 = 0.0;
 }
 
-function param(){
+function param() {
 
     //jellyfish
     this.jCount = 3;
@@ -33,22 +33,22 @@ function param(){
     this.jScaleRandom = 0;
     this.jTurb = 0.05;
     this.jSpeed = 0.01;
-    this.pBbox = [80,80,80];
+    this.pBbox = [ 80 , 80 , 80 ];
 
 }
 
-function setDebugParam(){
-    $("#jCount").val(Param.jCount);
-    $("#jScale").val(Param.jScale);
-    $("#jScaleRandom").val(Param.jScaleRandom);
-    $("#jTurb").val(Param.jTurb);
-    $("#jSpeed").val(Param.jSpeed);
+function setDebugParam() {
+    $( "#jCount" ).val( Param.jCount );
+    $( "#jScale" ).val( Param.jScale );
+    $( "#jScaleRandom" ).val( Param.jScaleRandom );
+    $( "#jTurb" ).val( Param.jTurb );
+    $( "#jSpeed" ).val( Param.jSpeed );
 }
 
-function readDebugParam(){
-    Param.jCount = parseFloat($("#jCount").val());
-    Param.jScale = parseFloat($("#jScale").val());
-    Param.jScaleRandom = parseFloat($("#jScaleRandom").val());
-    Param.jTurb = parseFloat($("#jTurb").val());
-    Param.jSpeed = parseFloat($("#jSpeed").val());
+function readDebugParam() {
+    Param.jCount = parseFloat( $( "#jCount" ).val() );
+    Param.jScale = parseFloat( $( "#jScale" ).val() );
+    Param.jScaleRandom = parseFloat( $( "#jScaleRandom" ).val() );
+    Param.jTurb = parseFloat( $( "#jTurb" ).val() );
+    Param.jSpeed = parseFloat( $( "#jSpeed" ).val() );
 }
