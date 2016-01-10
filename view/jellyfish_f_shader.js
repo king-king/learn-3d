96c93cb6c44462b8bab5194d1cdca4d793fd74aa
qunwang <niuyiwangqun@sohu.com>
2016-01-10 23:43:30 +0800

    c

--- view/jellyfish_f_shader.js
+++ view/jellyfish_f_shader.js
@@ -20,6 +20,5 @@ var f_shader=
     "void main(void) {\n"+
     "    vec3 caustics = texture2D(uSampler1, vec2((vWorld.x)/48.+uCurrentTime/12., (vWorld.z-vWorld.y)/95.)).rgb;\n"+
     "    vec4 colorMap = texture2D(uSampler0, vec2(vTextureCoord.s, vTextureCoord.t));\n"+
-
     "    gl_FragColor = vec4(((vDiffuse + caustics)*colorMap.rgb) + vFresnel, colorMap.a);\n"+
     "}\n";
